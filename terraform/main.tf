524db26ffb223e77932a10c728d96159b17e58b8
Aaron Earles <115801404+aaronearles@users.noreply.github.com>
2024-09-27 17:35:38 -0600

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,63 @@
+resource "linode_instance" "instance" {
+  label            = var.linode_label != "" ? var.linode_label : null
+  image            = var.linode_image
+  stackscript_id   = var.linode_stackscript_id != "" ? var.linode_stackscript_id : null
+  region           = var.linode_region
+  type             = var.linode_type
+  authorized_users = ["${var.linode_authorized_users}"]
+  firewall_id      = var.linode_firewall_id != "" ? var.linode_firewall_id : linode_firewall.firewall[0].id
+}
+
+output "instance_ip" {
+  value = linode_instance.instance.ip_address
+}
+
+resource "linode_firewall" "firewall" {
+  count = var.linode_firewall_id == "" ? 1 : 0
+  label = "${var.linode_label}_firewall"
+  inbound {
+    label    = "inbound-tcp"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = var.linode_firewall_ports
+    ipv4     = var.linode_firewall_source_ip
+  }
+
+  inbound_policy  = "DROP"
+  outbound_policy = "ACCEPT"
+}
+
+resource "cloudflare_record" "dns_record" {
+  count   = var.linode_label != "" ? 1 : 0
+  zone_id = var.cloudflare_zone_id
+  name    = var.linode_label
+  content = linode_instance.instance.ip_address
+  type    = "A"
+  ttl     = 60
+}
+
+################### ANSIBLE ######################
+
+# This resource will destroy (potentially immediately) after null_resource.next
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [linode_instance.instance]
+
+  create_duration = "30s"
+}
+
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_30_seconds]
+}
+
+resource "ansible_group" "terraform_all" {
+  name = "terraform_all"
+}
+
+resource "ansible_host" "terraform_host" {
+  name   = linode_instance.instance.ip_address
+  groups = [ansible_group.terraform_all.name]
+}
+
---
+++ terraform/provider.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    linode = {
+      source  = "linode/linode"
+      version = "~> 2.28.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.42.0"
+    }
+    ansible = {
+      version = "~> 1.3.0"
+      source  = "ansible/ansible"
+    }
+  }
+  backend "s3" {
+    endpoints = {
+      s3 = var.b2_s3_endpoint
+    }
+    access_key = var.b2_application_key_id
+    secret_key = var.b2_application_key
+    bucket     = var.b2_tfstate_bucket
+    key        = var.b2_tfstate_key
+    region     = "region" //This does nothing but is required by s3 backend.
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_s3_checksum            = true
+  }
+}
+
+provider "linode" {
+  token = var.linode_token
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
